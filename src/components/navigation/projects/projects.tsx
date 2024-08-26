afa0527523fa6416177e96ba4d9d37590c2f7528
apsarabishwokarma <apsarabk94@gmail.com>
2024-08-26 17:12:51 +0545

    feat: ui ,portfolio cards

--- src/components/navigation/projects/projects.tsx
+++ src/components/navigation/projects/projects.tsx
@@ -107,8 +107,8 @@ const SkeletonFour = () => {
         <p className="font-bold text-4xl text-white">BookNook</p>
         <p className="font-normal text-base text-white"></p>
         <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-          BookNook is a mobile application providing convenient book shopping
-          experience .
+          BookNook is a mobile application providing the joy of browsing and
+          purchasing books right to your fingertips.
         </p>
       </div>
 
