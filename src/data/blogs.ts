a8ec033700fa858229c5e49797f696fa98972848
apsarabishwokarma <apsarabk94@gmail.com>
2024-08-26 18:57:09 +0545

    feat: ui ,portfolio cards

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -9,28 +9,13 @@ export const blogs: BlogCardProps[] = [
     blogDetails: "javascript projects(Html,css,Js) ",
     readTime: "10",
   },
-  {
-    slug: "artificial-intelligence",
-    img: "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    date: "22/03/2023",
-    blogName: "Artificial Intelligence",
-    blogDetails: "Widely popular Artificial Intelligence",
-    readTime: "10",
-  },
+
   {
     slug: "roadmap-webdevelopment",
     img: "https://images.unsplash.com/photo-1484417894907-623942c8ee29?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     date: "02/09/2024",
     blogName: "Roadmap for Web-Development",
     blogDetails: "Learn Web development step by step",
-    readTime: "10",
-  },
-  {
-    slug: "github",
-    img: "https://images.unsplash.com/photo-1618401479427-c8ef9465fbe1?q=80&w=2043&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    date: "22/03/2023",
-    blogName: "Github",
-    blogDetails: "Widely popular Source code management",
     readTime: "10",
   },
   {
@@ -43,6 +28,15 @@ export const blogs: BlogCardProps[] = [
     readTime: "12",
   },
   {
+    slug: "github",
+    img: "https://images.unsplash.com/photo-1618401479427-c8ef9465fbe1?q=80&w=2043&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    date: "22/03/2023",
+    blogName: "Github",
+    blogDetails: "Widely popular Source code management",
+    readTime: "10",
+  },
+
+  {
     slug: "frontend-developer-preparation",
     img: "https://images.unsplash.com/photo-1620632889724-f1ddc7841c40?",
     date: "04/25/2024",
@@ -50,6 +44,14 @@ export const blogs: BlogCardProps[] = [
       "Frontend Developer Interview Preparation: Essential Tips and Resources",
     blogDetails:
       "Get ready for your frontend developer interviews with these essential tips and resources.HTML,CSS,JS",
+    readTime: "10",
+  },
+  {
+    slug: "artificial-intelligence",
+    img: "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    date: "22/03/2023",
+    blogName: "Artificial Intelligence",
+    blogDetails: "Widely popular Artificial Intelligence",
     readTime: "10",
   },
 ];
