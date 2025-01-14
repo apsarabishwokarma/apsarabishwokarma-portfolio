52ea4be7591762546afa4ec2d9b6e7186a8bb3cc
apsarabishwokarma <apsarabk94@gmail.com>
2025-01-14 14:53:59 +0545

    ui refactored

---
+++ src/data/portfolio-pendinglist.ts
@@ -0,0 +1,27 @@
+const portfolioList = [
+  {
+    image: "https://cdn.easyfrontend.com/pictures/portfolio/portfolio_1_1.png",
+    title: "Photography",
+    categories: ["Cinematography"],
+    link: "https://eurekainternational.com.np/",
+  },
+  {
+    image: "https://cdn.easyfrontend.com/pictures/portfolio/portfolio_1_2.png",
+    title: "Web Portal Dev",
+    categories: ["Product Design"],
+    link: "https://shophub-ecommerce.vercel.app/",
+  },
+  {
+    image: "https://cdn.easyfrontend.com/pictures/portfolio/portfolio_1_3.png",
+    title: "Marketing",
+    categories: ["Digital Marketing"],
+    link: "https://apsarabishwokarma.github.io/javascript-projects/",
+  },
+  {
+    image: "https://cdn.easyfrontend.com/pictures/portfolio/portfolio_1_4.png",
+    title: "Web Portal Dev",
+    categories: ["UI/UX Design"],
+    link: "https://github.com/Roopaish/evento",
+  },
+];
+export default portfolioList;
